c4387010f4eaa6c22c78faf4f5ab546fcceb3cd1
SilvioDoMine <arthurhenrique@live.com>
2025-09-18 14:07:36 -0300

    removendo hud

--- app/components/Hud.vue
+++ app/components/Hud.vue
@@ -9,7 +9,6 @@ const runStore = useRunStore();
 
 <template>
     <div v-if="[GameStateEnum.IN_GAME_1, GameStateEnum.IN_GAME_2].includes(gameStateStore.state)" class="pointer-events-none absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center">
-        <h1 class="text-white text-4xl animate-pulse">HUD Component - To be implemented</h1>
 
         <!-- Create absolute div that display axys X Y Z on the screen to help me -->
         <div class="absolute top-2 left-2 text-white text-xs p-2 bg-black/50 rounded">
