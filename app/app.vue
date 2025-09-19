28ead8cecc09a58663331629095bd67ddac76058
SilvioDoMine <arthurhenrique@live.com>
2025-09-18 23:47:12 -0300

    Revert "adiciona conteúdo de pwa"
    This reverts commit 9f47ef8d75610d82cf33bf4ecabe12d538e59a65.

--- app/app.vue
+++ app/app.vue
@@ -7,41 +7,10 @@ const game = useGameStore();
 
 // Setup input event listeners
 inputStore.setupEventListeners();
-
-const toggleFullscreen = async () => {
-  try {
-    // Para iOS Safari, usa webkitRequestFullscreen
-    const docElement = document.documentElement as any;
-
-    if (!document.fullscreenElement && !docElement.webkitFullscreenElement) {
-      if (docElement.requestFullscreen) {
-        await docElement.requestFullscreen();
-      } else if (docElement.webkitRequestFullscreen) {
-        await docElement.webkitRequestFullscreen();
-      } else if (docElement.mozRequestFullScreen) {
-        await docElement.mozRequestFullScreen();
-      } else if (docElement.msRequestFullscreen) {
-        await docElement.msRequestFullscreen();
-      }
-    } else {
-      if (document.exitFullscreen) {
-        await document.exitFullscreen();
-      } else if ((document as any).webkitExitFullscreen) {
-        await (document as any).webkitExitFullscreen();
-      } else if ((document as any).mozCancelFullScreen) {
-        await (document as any).mozCancelFullScreen();
-      } else if ((document as any).msExitFullscreen) {
-        await (document as any).msExitFullscreen();
-      }
-    }
-  } catch (error) {
-    console.log('Fullscreen não suportado ou bloqueado');
-  }
-};
 </script>
 
 <template>
-  <div class="bg-black" @click="toggleFullscreen" @touchend="toggleFullscreen">
+  <div class="bg-black">
     <GameCanvas />
 
     <!-- HTML UI -->
