9ef25f718ad4212a03352efc7af52a9646c3b8b4
SilvioDoMine <arthurhenrique@live.com>
2025-09-18 23:33:09 -0300

    ajustando viewport

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,7 @@ export default defineNuxtConfig({
       meta: [
         {
           name: 'viewport',
-          content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover'
+          content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, viewport-fit=cover, shrink-to-fit=no'
         }
       ]
     }
