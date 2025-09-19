cd6a5208e1f20d5f3b2067bee7914ba0b54609e0
SilvioDoMine <arthurhenrique@live.com>
2025-09-18 21:27:06 -0300

    Clique também continua

--- app/components/MainMenu.vue
+++ app/components/MainMenu.vue
@@ -1,18 +1,23 @@
 <script setup lang="ts">
 import { useGameStore } from "~/stores/game";
-import { useInputStore } from "~/stores/input";
+import { useInputStore, InputState } from "~/stores/input";
 import { GameStateEnum } from "~/interfaces/types";
 
 const inputStore = useInputStore();
 const gameStateStore = useGameStore();
 
-watch(() => inputStore.state.keyboard, (keys: { keys: string[] }) => {
-  if (keys.keys.has(" ") || keys.keys.has("Enter")) {
-    if (gameStateStore.state === GameStateEnum.MAIN_MENU) {
-      gameStateStore.state = GameStateEnum.LOBBY;
-    }
+const unwatch = watch(() => inputStore.state, (inputState: InputState) => {
+  if (gameStateStore.state !== GameStateEnum.MAIN_MENU) return;
+
+  if (inputState.keyboard.keys.size > 0 || inputState.mouse.isDown ) {
+    gameStateStore.state = GameStateEnum.LOBBY;
   }
-}, { deep: true });
+}, {deep: true});
+
+
+onUnmounted(() => {
+  unwatch();
+});
 </script>
 
 <template>
@@ -20,6 +25,6 @@ watch(() => inputStore.state.keyboard, (keys: { keys: string[] }) => {
     v-if="gameStateStore.state === GameStateEnum.MAIN_MENU"
     class="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center"
     >
-    <h1 class="text-white text-4xl animate-pulse">Press Enter to continue</h1>
+    <h1 class="text-white text-4xl animate-pulse">Press anything to start...</h1>
   </div>
 </template>
